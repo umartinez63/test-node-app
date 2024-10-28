c89a56b575062152248f1a8f89f241b5dd393985
umartinez63 <umartinez@cox.net>
2024-10-28 07:02:36 -0700

    Created app.js
    Changed main to be app.js, created app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+//
+// First project using node
+//
+
+const forFun = ["hello", "goodbye", "you say yes", "I say no", "I don't know"];
+
+console.log ("Hello World");
+for (let fun of forFun)
+  console.log (fun);
